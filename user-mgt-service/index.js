b138f3ed5564fab8013cae08df1512190206c053
TaimoorIkram <tikram.bese21seecs@seecs.edu.pk>
2023-12-23 13:31:26 +0500

    Modify auth routes and remove redundant and unnecessary routes.

--- user-mgt-service/index.js
+++ user-mgt-service/index.js
@@ -6,17 +6,19 @@ const user_router = require('./users');
 const app = express();
 
 app.use('/users', user_router);
-
-app.get('/', function (req, res) {
-    res.send(
-        "This url now works!"
-    );
-})
+app.get('/routes', (req, res) => {
+    let routes = [];
+    user_router.stack.forEach((object) => {
+        if(object.route) routes.push(object.route.path);
+    })
+    console.log(routes);
+    res.status(200).json(routes);
+});
 
 app.get('/*', (req, res) => {
-    res.send({"detail": "The requested resouce couldn't be found."}).status(404);
+    res.status(404).send({"error": "The requested resouce couldn't be found."});
 })
 
 app.listen(process.env.PORT, () => {
-    console.log('This server is now listening. Go give it a whirl!');
+    console.log(`This server is now listening on port ${process.env.PORT}. Go give it a whirl!`);
 })
