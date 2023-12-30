de74ae5978271a068d21fd44c199976936c5398d
raouzair10 <muzair.bese21seecs@seecs.edu.pk>
2023-12-31 02:36:56 +0500

    Update signup.js

--- frontend-service/src/pages/cloud/login.js
+++ frontend-service/src/pages/cloud/login.js
@@ -39,24 +39,7 @@ const Login = () => {
           </form>
         </div>
         </div>
-      );    
-
-    return (
-        <div>
-            <div>
-                <h2>Login</h2>
-                <form onSubmit={handleLogin}>
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} name="username" />
-
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" />
-
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-        </div>
-    );
+      );
 };
 
 export default Login;
--- frontend-service/src/pages/cloud/signup.js
+++ frontend-service/src/pages/cloud/signup.js
@@ -1,79 +1,66 @@
-import React, { useState } from 'react';
-import styles from '@/styles/SignupPage.module.css'; // Add your styles file
+import React, { useContext, useEffect, useState } from 'react';
+import AuthContext from '@/contexts/AuthContext';
+import styles from '@/styles/SignupPage.module.css';
+import { useRouter } from 'next/router';
 
 const Signup = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
+    const router = useRouter();
+    const { user, registerUser } = useContext(AuthContext);
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
     });
-  };
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
+    useEffect(() => {
+        if (user) {
+            console.log("hello");
+            router.push("/cloud");
+        }
+    }, [user, router]);
 
-//     // Validate the form data before submitting (e.g., check password match, email format, etc.)
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
 
-//     // Assuming you have an API endpoint for user registration
-//     try {
-//       const response = await fetch('YOUR_API_ENDPOINT/signup', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(formData),
-//       });
+    const handleSignup = (e) => {
+        // registerUser(e);
+    };
 
-//       if (response.ok) {
-//         console.log('User registered successfully');
-//         // Optionally, redirect the user to a login page or handle success in another way
-//       } else {
-//         console.error('Failed to register user');
-//         // Optionally, handle the error and provide feedback to the user
-//       }
-//     } catch (error) {
-//       console.error('Error during registration:', error);
-//       // Optionally, handle the error and provide feedback to the user
-//     }
-//   };
-
-return (
-    <div className={styles.page}>
-    <div className={styles.signupContainer}>
-      <h1 className={styles.heading}>Sign Up</h1>
-      <form onSubmit={() => {}} className={styles.form}>
-        <label className={styles.label}>
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label className={styles.label}>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <button className={styles.button} type="submit">
-          Sign Up
-        </button>
-      </form>
-    </div>
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <div className={styles.signupContainer}>
+                <h1 className={styles.heading}>Sign Up</h1>
+                <form onSubmit={handleSignup} className={styles.form}>
+                    <label className={styles.label}>
+                        Username:
+                        <input
+                            type="text"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            required
+                        />
+                    </label>
+                    <label className={styles.label}>
+                        Password:
+                        <input
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+                    </label>
+                    <button className={styles.button} type="submit">
+                        Sign Up
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Signup;
