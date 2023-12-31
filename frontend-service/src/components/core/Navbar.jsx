635eb34f51b58e02c1bf0245daedbe0dab8534ed
raouzair10 <muzair.bese21seecs@seecs.edu.pk>
2023-12-31 20:57:31 +0500

    Add the functionality to update navbar dynamically

--- frontend-service/src/components/core/Navbar.jsx
+++ frontend-service/src/components/core/Navbar.jsx
@@ -1,10 +1,25 @@
 import AuthContext from "@/contexts/AuthContext";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useContext } from "react";
 import styles from "@/styles/NavbarStyles.module.css";
 
 export default function Navbar() {
   const { user, logoutUser } = useContext(AuthContext);
+  const router = useRouter();
+
+  // Function to determine the correct text for the button based on the current route
+  const getButtonLabel = () => {
+    const { pathname } = router;
+
+    if (pathname === "/cloud/login") {
+      return "Sign Up";
+    } else if (pathname === "/cloud/signup") {
+      return "Login";
+    }
+
+    return user ? "Dashboard" : "Login";
+  };
 
   return (
     <nav className={styles.navbar}>
@@ -14,9 +29,11 @@ export default function Navbar() {
         </Link>
       </div>
       <div className={styles.links}>
-        <Link href="/cloud">
-          <h1 className={styles.navLink}>Dashboard</h1>
-        </Link>
+        {user && (
+          <Link href="/cloud">
+            <h1 className={styles.navLink}>Dashboard</h1>
+          </Link>
+        )}
       </div>
       <div className={styles.actions}>
         {user ? (
@@ -27,8 +44,8 @@ export default function Navbar() {
             </div>
           </div>
         ) : (
-          <Link href="/cloud/login">
-            <h1 className={styles.navLink}>Login</h1>
+          <Link href={`/cloud/${getButtonLabel().toLowerCase().replace(/\s/g, '')}`}>
+            <h1 className={styles.navLink}>{getButtonLabel()}</h1>
           </Link>
         )}
       </div>
