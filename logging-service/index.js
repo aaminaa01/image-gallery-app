6c1a9aa9cc8157f3786193e6785ce5e76e3f9eca
Fatima Binte Tanveer <fatimabintetanveer@gmail.com>
2023-12-31 17:23:10 +0500

    Modify Ports

--- logging-service/index.js
+++ logging-service/index.js
@@ -4,13 +4,14 @@ const express = require('express');
 const logs_router = require('./logs');
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use('/logs', logs_router);
 
 app.get('/*', (req, res) => {
-    res.status(404).send({"error": "The requested resouce couldn't be found."});
+    res.status(404).send({"error": "The requested resource couldn't be found."});
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`This server is now listening on port ${process.env.PORT}. Go give it a whirl!`);
-})
+app.listen(port, () => {
+    console.log(`This server is now listening on port ${port}. Go give it a whirl!`);
+});
